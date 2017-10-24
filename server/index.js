39b2cbcfbe85d573822cc73b903cca52a7cd2264
Kenny Cao <kennyxcao@gmail.com>
2017-10-24 15:51:02 -0700

    Add some npm packages and create index.js for server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 2244;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log('Listening on port 2244');
+});
+
+module.exports = {
+  app
+};
