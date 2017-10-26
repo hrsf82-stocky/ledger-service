c0ba90bcf92432ded7c9f225a7121a6b5558ce55
Kenny Cao <kennyxcao@gmail.com>
2017-10-26 14:51:56 -0700

    Implement csv to array function to pull sample price data

--- simulator/priceQueuePusher.js
+++ simulator/priceQueuePusher.js
@@ -1,8 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const csv = require('csv');
 const AWS = require('aws-sdk');
 
 AWS.config.loadFromPath('../config.json');
 
-// Create an SQS service object
+const csvFilePath = path.join(__dirname, '/EURUSD-Sample-Prices.csv');
 const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 
 // List Existing Queue URLS
@@ -14,42 +17,69 @@ const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 //   }
 // });
 
-const params = {
-  DelaySeconds: 10,
-  MessageAttributes: {
-    time: {
-      DataType: 'String',
-      StringValue: '2017-10-01 21:00:06.101000'
-    },
-    instrument: {
-      DataType: 'String',
-      StringValue: 'EURUSD'
-    },
-    bid: {
-      DataType: 'Number',
-      StringValue: '1.13015'
-    },
-    ask: {
-      DataType: 'Number',
-      StringValue: '1.13028'
-    },
-    bid_vol: {
-      DataType: 'Number',
-      StringValue: '750000'
-    },
-    ask_vol: {
-      DataType: 'Number',
-      StringValue: '1500000'
-    }
-  },
-  MessageBody: 'Sample Data',
-  QueueUrl: 'https://sqs.us-west-1.amazonaws.com/287396276472/SQS_PRICES_QUEUE'
+// const params = {
+//   DelaySeconds: 10,
+//   MessageAttributes: {
+//     time: {
+//       DataType: 'String',
+//       StringValue: '2017-10-01 21:00:06.101000'
+//     },
+//     instrument: {
+//       DataType: 'String',
+//       StringValue: 'EURUSD'
+//     },
+//     bid: {
+//       DataType: 'Number',
+//       StringValue: '1.13015'
+//     },
+//     ask: {
+//       DataType: 'Number',
+//       StringValue: '1.13028'
+//     },
+//     bid_vol: {
+//       DataType: 'Number',
+//       StringValue: '750000'
+//     },
+//     ask_vol: {
+//       DataType: 'Number',
+//       StringValue: '1500000'
+//     }
+//   },
+//   MessageBody: 'Sample Data',
+//   QueueUrl: 'https://sqs.us-west-1.amazonaws.com/287396276472/SQS_PRICES_QUEUE'
+// };
+
+// sqs.sendMessage(params, (err, data) => {
+//   if (err) {
+//     console.log('Error', err);
+//   } else {
+//     console.log('Success', data.MessageId);
+//   }
+// });
+
+const parseCVSPrices = (path, callback) => {
+  const options = {
+    delimiter: ',',
+    trim: true,
+    columns: ['time', 'bid', 'ask', 'bid_vol', 'ask_vol']
+  };
+  const priceData = [];
+
+  fs.createReadStream(path)
+    .pipe(csv.parse(options))
+    .on('data', (csvrow) => {
+      priceData.push(csvrow);
+    })
+    .on('end', () => {
+      console.log('Finished parsing all files');
+      callback(priceData);
+    })
+    .on('error', (err) => {
+      console.error(err);
+    });
 };
 
-sqs.sendMessage(params, (err, data) => {
-  if (err) {
-    console.log('Error', err);
-  } else {
-    console.log('Success', data.MessageId);
-  }
+
+parseCVSPrices(csvFilePath, (priceData) => {
+  console.log(priceData);
 });
