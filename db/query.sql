3bb2a7afbbccb65eaa70ad0ad689cbb7898effa1
Kenny Cao <kennyxcao@gmail.com>
2017-10-28 02:28:58 -0700

    Update query  to reflect new database schema

--- db/query.sql
+++ db/query.sql
@@ -1,20 +1,20 @@
 -- Calculated Average Price
-SELECT  
-    date_trunc('hour', dt) dt,
-    ((array_agg(bid ORDER BY dt ASC))[1] +
-     (array_agg(ask ORDER BY dt ASC))[1])/2 o,
-    (MAX(bid) + MAX(ask))/2 h,
-    (MIN(bid) + MIN(ask))/2 l,
-    ((array_agg(bid ORDER BY dt DESC))[1] +
-     (array_agg(ask ORDER BY dt DESC))[1])/2 c,
-    SUM(bid_vol) bid_vol,
-    SUM(ask_vol) ask_vol,
-    COUNT(*) ticks
-FROM EURUSD  
-WHERE dt >= '2017-07-01' 
-GROUP BY date_trunc('hour', dt)  
-ORDER BY dt DESC
-LIMIT 5;
+-- SELECT  
+--     date_trunc('hour', dt) dt,
+--     ((array_agg(bid ORDER BY dt ASC))[1] +
+--      (array_agg(ask ORDER BY dt ASC))[1])/2 o,
+--     (MAX(bid) + MAX(ask))/2 h,
+--     (MIN(bid) + MIN(ask))/2 l,
+--     ((array_agg(bid ORDER BY dt DESC))[1] +
+--      (array_agg(ask ORDER BY dt DESC))[1])/2 c,
+--     SUM(bid_vol) bid_vol,
+--     SUM(ask_vol) ask_vol,
+--     COUNT(*) ticks
+-- FROM EURUSD  
+-- WHERE dt >= '2017-07-01' 
+-- GROUP BY date_trunc('hour', dt)  
+-- ORDER BY dt DESC
+-- LIMIT 5;
 
 -- Bid Price
 -- SELECT  
@@ -29,4 +29,25 @@ LIMIT 5;
 -- FROM EURUSD  
 -- WHERE dt BETWEEN '2017-07-01' AND '2017-10-24'  
 -- GROUP BY date_trunc('hour', dt)  
--- ORDER BY dt  
+-- ORDER BY dt;
+
+WITH intervals AS (
+  SELECT start, start + interval '1hour' AS end
+  FROM generate_series('2017-07-01 12:00', '2017-10-27 12:00', interval '1 hour') AS start)
+SELECT DISTINCT
+  intervals.start AS date,
+  min(bid_l) OVER w AS bid_low,
+  max(bid_h) OVER w AS bid_high,
+  first_value(bid_o) OVER w as bid_open,
+  last_value(bid_c) OVER w as bid_close,
+  sum(bid_v) OVER w AS bid_vol,
+  sum(ticks) OVER w AS ticks
+FROM intervals 
+JOIN s5bars s5 ON 
+  s5.id_pairs = 1 AND
+  s5.dt >= intervals.start AND
+  s5.dt < intervals.end
+WINDOW w AS (PARTITION BY intervals.start ORDER BY
+  s5.dt ASC rows BETWEEN UNBOUNDED PRECEDING AND
+  UNBOUNDED FOLLOWING)
+ORDER BY intervals.start;
