8157ae6ed9ba0bcf30cad8d43f1dc63a080e7500
Kenny Cao <kennyxcao@gmail.com>
2017-10-30 16:24:49 -0700

    Refactor server-test to new repo file structure

--- test/server-test.js
+++ test/server-test.js
@@ -1,24 +1,43 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const { expect } = require('chai');
 const request = require('supertest');
-const server = require('../server/index.js');
+const app = require('../app.js');
 
+chai.use(chaiHttp);
+
+const server = app.listen(8888);
 
 describe('Server Specs', () => {
   beforeEach(() => {
-    // server.listen(8888);
+    server.listen(8888);
   });
 
   afterEach(() => {
-    // server.close();
+    server.close();
   });
 
-  describe('GET /', () => {
+  describe('GET /test', () => {
     it('should return 200 status code and matching response ', (done) => {
       request(server)
-        .get('/')
+        .get('/test')
         .expect(200)
         .expect((res) => {
-          expect(res.body).to.be.equal('Hello World');
+          expect(res.body).to.be.equal('Hello World Test');
+        })
+        .end(done);
+    });
+  });
+
+  describe('GET /test', () => {
+    it('should return 200 status code and matching response ', (done) => {
+      request(server)
+        .get('/test')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).to.be.equal('Hello World Test');
         })
         .end(done);
     });
