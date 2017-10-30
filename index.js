9f7d2fa215f399f9598d96230202b466a492dadc
Kenny Cao <kennyxcao@gmail.com>
2017-10-30 16:00:55 -0700

    Add template files for all controllers and verify controller is working

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8888;
 
 app.listen(port, (err) => {
   if (err) {
-    console.error('Cannot Connect to Ledger Server');
+    console.error('Cannot Connect to Ledger Service Server');
   }
-  console.log(`Ledger Server: Listening on ${port}`);
+  console.log(`Ledger Service Server: Listening on ${port}`);
 });
