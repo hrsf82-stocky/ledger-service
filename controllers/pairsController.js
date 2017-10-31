a4a758c4894a41d1d64298682989420146d9d2b4
Kenny Cao <kennyxcao@gmail.com>
2017-10-31 01:04:33 -0700

    Implement get post delete and patch rest API for pairs end point

--- controllers/pairsController.js
+++ controllers/pairsController.js
@@ -6,26 +6,53 @@ module.exports = {
 
     if (pairID !== undefined) {
       queries.getPair({ id: pairID })
-        .then(pair => res.status(200).json(pair));
+        .then(pair => res.status(200).json(pair))
+        .catch((err) => {
+          console.error(err);
+          res.status(404).json(err);
+        });
     } else {
       queries.getAllPairs()
-        .then(pairs => res.status(200).json(pairs));
+        .then(pairs => res.status(200).json(pairs))
+        .catch((err) => {
+          console.error(err);
+          res.status(404).json(err);
+        });
     }
   },
+
   post: (req, res) => {
-    
-    // models.messages.post(req.body, function(error, results) {
-    //   res.status(error ? 400 : 201).send();
-    // });
+    const { name, major } = req.body;
+
+    queries.addPair({ name, major })
+      .then(newPair => res.status(201).json(newPair))
+      .catch((err) => {
+        console.error(err);
+        res.status(400).json(err);
+      });
   },
+
   delete: (req, res) => {
-    // models.messages.post(req.body, function(error, results) {
-    //   res.status(error ? 400 : 201).send();
-    // });
+    const pairID = req.params.id;
+
+    queries.deletePairById(pairID)
+      .then(result => res.status(200).json(result))
+      .catch((err) => {
+        console.error(err);
+        res.status(400).json(err);
+      });
   },
+
   patch: (req, res) => {
-    // models.messages.post(req.body, function(error, results) {
-    //   res.status(error ? 400 : 201).send();
-    // });
+    const pairID = req.params.id;
+    const updates = req.body;
+    console.log(pairID);
+
+    queries.updatePairById(pairID, updates)
+      .then(updatedPair => res.status(200).json(updatedPair))
+      .catch((err) => {
+        console.error(err);
+        res.status(400).json(err);
+      });
   }
 };
