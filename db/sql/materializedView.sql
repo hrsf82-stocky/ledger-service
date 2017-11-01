83aa3871a6d0ab784555bdee40b19360ff848f5c
Kenny Cao <kennyxcao@gmail.com>
2017-10-31 17:27:14 -0700

    Refactor materialized view query to reflect database changes

--- db/sql/materializedView.sql
+++ db/sql/materializedView.sql
@@ -1,15 +1,25 @@
 CREATE MATERIALIZED VIEW eurusd_m1
 AS
   WITH intervals AS (
-    SELECT start, start + interval '1hour' AS end
-    FROM generate_series('2017-07-01 12:00', '2017-10-27 12:00', interval '1 hour') AS start)
+    SELECT start, start + interval '1 minute' AS end
+  FROM generate_series(current_date - interval '1 month', current_date, interval '1 minute') AS start)
   SELECT DISTINCT
-    intervals.start AS date,
-    min(bid_l) OVER w AS bid_low,
-    max(bid_h) OVER w AS bid_high,
-    first_value(bid_o) OVER w as bid_open,
-    last_value(bid_c) OVER w as bid_close,
-    sum(bid_v) OVER w AS bid_vol,
+    intervals.start AS dt,
+    min(bid_l) OVER w AS bid_l,
+    max(bid_h) OVER w AS bid_h,
+    first_value(bid_o) OVER w as bid_o,
+    last_value(bid_c) OVER w as bid_c,
+    sum(bid_v) OVER w AS bid_v,
+    min(ask_l) OVER w AS ask_l,
+    max(ask_h) OVER w AS ask_h,
+    first_value(ask_o) OVER w as ask_o,
+    last_value(ask_c) OVER w as ask_c,
+    sum(ask_v) OVER w AS ask_v,
+    min((bid_l + ask_l) / 2) OVER w AS mid_l,
+    max((bid_h + ask_h) / 2) OVER w AS mid_h,
+    first_value((bid_o + ask_o) / 2) OVER w as mid_o,
+    last_value((bid_c + ask_c) / 2) OVER w as mid_c,
+    sum((bid_v + ask_v) / 2) OVER w AS mid_v,
     sum(ticks) OVER w AS ticks
   FROM intervals 
   JOIN s5bars s5 ON 
