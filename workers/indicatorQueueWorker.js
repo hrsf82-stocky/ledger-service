d7e628932330b8b138c0bd4942b931fef2a15299
Kenny Cao <kennyxcao@gmail.com>
2017-11-02 11:55:01 -0700

    Install lodash npm module

--- workers/indicatorQueueWorker.js
+++ workers/indicatorQueueWorker.js
@@ -1,5 +1,9 @@
 const cron = require('cron');
+const moment = require('moment');
 const redisClient = require('../db/redisClient');
+
+const redisSSKey = 's5bars';
+
 
 /**
  * Runs Every 5 seconds
@@ -8,8 +12,14 @@ const job = new cron.CronJob({
   cronTime: '*/5 * * * * *',
   onTick: () => {
     console.log('Indicator Queue Worker Ticked');
+    const currentUnix = moment().utc().unix();
 
-
+    // Retrieve all ticks row ids with score up to the last 5 second interval mark
+    redisClient.zrangeAsync(redisSSKey, 0, currentUnix, 'WITHSCORES')
+      .then((reply) => {
+        console.log(reply);
+      })
+      .catch(console.error);
   },
   start: false,
   timeZone: 'America/Los_Angeles'
