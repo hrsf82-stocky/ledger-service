c83dd7676f8cc2645a0fa1dc06dd2e2553a91cbe
Kenny Cao <kennyxcao@gmail.com>
2017-11-03 23:51:34 -0700

    Add db test spec file

---
+++ test/db-spec.js
@@ -0,0 +1,45 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { expect } = require('chai');
+const request = require('supertest');
+const app = require('../app.js');
+
+chai.use(chaiHttp);
+
+const server = app.listen(8888);
+
+describe('Server Specs', () => {
+  beforeEach(() => {
+    server.listen(8888);
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
+  describe('GET /test', () => {
+    it('should return 200 status code and matching response ', (done) => {
+      request(server)
+        .get('/test')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).to.be.equal('Hello World Test');
+        })
+        .end(done);
+    });
+  });
+
+  describe('GET /', () => {
+    it('should return 404 status code', (done) => {
+      request(server)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body.message).to.be.equal('Not Found');
+        })
+        .end(done);
+    });
+  });
+});
