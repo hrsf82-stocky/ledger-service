0c963ebe7091bdd104f5fac5018fbb1cbc910fdc
Kenny Cao <kennyxcao@gmail.com>
2017-11-09 11:47:43 -0800

    Fix mviews delete and patch logic to check the params name for all operations

--- controllers/mviewsController.js
+++ controllers/mviewsController.js
@@ -19,12 +19,12 @@ module.exports = {
   delete: (req, res, next) => {
     const mviewName = req.params.name;
 
-    if (mviewName) {
-      queries.deleteMviewByName(mviewName)
+    if (mviewName === 'all') {
+      queries.deleteAllMviews()
         .then(result => res.status(200).json(result))
         .catch(err => next(err));
     } else {
-      queries.deleteAllMviews()
+      queries.deleteMviewByName(mviewName)
         .then(result => res.status(200).json(result))
         .catch(err => next(err));
     }
@@ -34,12 +34,12 @@ module.exports = {
     const mviewName = req.params.name;
     const { concurrent } = req.body;
 
-    if (mviewName) {
-      queries.refreshMviewByName(mviewName, !!concurrent)
+    if (mviewName === 'all') {
+      queries.refreshAllMviews(!!concurrent)
         .then(result => res.status(200).json(result))
         .catch(err => next(err));
     } else {
-      queries.refreshAllMviews(!!concurrent)
+      queries.refreshMviewByName(mviewName, !!concurrent)
         .then(result => res.status(200).json(result))
         .catch(err => next(err));
     }
