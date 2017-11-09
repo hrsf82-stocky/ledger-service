184f61b890cfa54865d74632620ef09e2034cca1
Kenny Cao <kennyxcao@gmail.com>
2017-11-08 19:22:00 -0800

    Add statsD to monitor latency and performance metrics for all workers

--- workers/histQueuePoll.js
+++ workers/histQueuePoll.js
@@ -1,42 +1,120 @@
 const AWS = require('aws-sdk');
-const Consumer = require('sqs-consumer');
+// const Consumer = require('sqs-consumer');
 const { HistQueueURL } = require('../config.js');
 const histProcessor = require('../lib/histProcessor');
+const statsDClient = require('../db/statsDClient');
 
 AWS.config.loadFromPath('../config.json');
+AWS.config.setPromisesDependency(require('bluebird'));
 
-const app = Consumer.create({
-  queueUrl: HistQueueURL,
-  attributeNames: ['All'],
-  messageAttributeNames: ['All'],
-  visibilityTimeout: 120,
-  waitTimeSeconds: 15,
-  sqs: new AWS.SQS(),
-  handleMessage: (message, done) => {
-    // console.log(message.MessageId);
-    // console.log(message.Attributes);
-    // console.log(message.MessageAttributes);
-    const { payload } = JSON.parse(message.Body);
-    const histRequest = Array.isArray(payload) ? payload[0] : payload;
-    histRequest.messageID = message.MessageId;
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 
-    console.log(histRequest);
+const params = {
+  AttributeNames: [
+    'All'
+  ],
+  MaxNumberOfMessages: 1,
+  MessageAttributeNames: [
+    'All'
+  ],
+  QueueUrl: process.env.HistQueueURL || HistQueueURL,
+  VisibilityTimeout: 120,
+  WaitTimeSeconds: 15
+};
 
-    histProcessor(histRequest)
-      .then(res => done())
-      .catch((err) => {
-        console.error(err);
-        done();
-      });
-  }
-});
+const deleteSQSMessage = (message) => {
+  const deleteParams = {
+    QueueUrl: process.env.HistQueueURL || HistQueueURL,
+    ReceiptHandle: message.ReceiptHandle
+  };
 
-app.on('error', (err) => {
-  console.error(err.message);
-});
+  return sqs.deleteMessage(deleteParams).promise();
+};
+
+const pollHistQueueMessage = () => {
+  sqs.receiveMessage(params).promise()
+    .then((res) => {
+      if (!res.Messages) {
+        throw new Error('No New SQS Historical OHLC Request Message');
+      } else {
+        return res.Messages[0];
+      }
+    })
+    .then((message) => {
+      const start = Date.now();
+      statsDClient.increment('.ledger.hist.received');
+      // console.log(message.MessageId);
+      // console.log(message.Attributes);
+      // console.log(message.MessageAttributes);
+      const { payload } = JSON.parse(message.Body);
+      const histRequest = Array.isArray(payload) ? payload[0] : payload;
+      histRequest.messageID = message.MessageId;
+
+      console.log(histRequest);
+
+      histProcessor(histRequest)
+        .then((res) => {
+          statsDClient.increment('.ledger.hist.processed.success');
+          statsDClient.timing('.ledger.hist.processed.success.latency_ms', Date.now() - start);
+
+          return deleteSQSMessage(message);
+        })
+        .catch((err) => {
+          statsDClient.increment('.ledger.hist.processed.fail');
+          statsDClient.timing('.ledger.hist.processed.fail.latency_ms', Date.now() - start);
+          console.error(err);
+
+          return deleteSQSMessage(message);
+        })
+        .then(() => {
+          console.log(`Message Deleted: ${message.MessageId}`);
+          pollHistQueueMessage();
+        })
+        .catch(() => {
+          console.error(`Message Delete Error: ${message.MessageId}`);
+          pollHistQueueMessage();
+        });
+    })
+    .catch((err) => {
+      console.error(err.message);
+      setTimeout(pollHistQueueMessage, 50);
+    });
+};
+
+// SQS Consumer Implementation - not fast enough
+// const app = Consumer.create({
+//   queueUrl: HistQueueURL,
+//   attributeNames: ['All'],
+//   messageAttributeNames: ['All'],
+//   visibilityTimeout: 120,
+//   waitTimeSeconds: 15,
+//   sqs: new AWS.SQS(),
+//   handleMessage: (message, done) => {
+//     // console.log(message.MessageId);
+//     // console.log(message.Attributes);
+//     // console.log(message.MessageAttributes);
+//     const { payload } = JSON.parse(message.Body);
+//     const histRequest = Array.isArray(payload) ? payload[0] : payload;
+//     histRequest.messageID = message.MessageId;
+
+//     console.log(histRequest);
+
+//     histProcessor(histRequest)
+//       .then(res => done())
+//       .catch((err) => {
+//         console.error(err);
+//         done();
+//       });
+//   }
+// });
+
+// app.on('error', (err) => {
+//   console.error(err.message);
+// });
+//  app.start();
 
 if (!module.parent) {
-  app.start();
+  pollHistQueueMessage();
 }
 
-module.exports = app;
+module.exports = pollHistQueueMessage;
--- workers/priceQueuePoll.js
+++ workers/priceQueuePoll.js
@@ -1,40 +1,123 @@
 const AWS = require('aws-sdk');
-const Consumer = require('sqs-consumer');
+// const Consumer = require('sqs-consumer');
 const { PriceQueueURL } = require('../config.js');
 const priceProcessor = require('../lib/priceProcessor');
+const statsDClient = require('../db/statsDClient');
 
 AWS.config.loadFromPath('../config.json');
+AWS.config.setPromisesDependency(require('bluebird'));
 
-const app = Consumer.create({
-  queueUrl: PriceQueueURL,
-  attributeNames: ['All'],
-  messageAttributeNames: ['All'],
-  visibilityTimeout: 120,
-  waitTimeSeconds: 15,
-  sqs: new AWS.SQS(),
-  handleMessage: (message, done) => {
-    // console.log(message.MessageId);
-    // console.log(message.Attributes);
-    // console.log(message.MessageAttributes);
-    const price = JSON.parse(message.Body).payload;
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 
-    console.log(price);
+const params = {
+  AttributeNames: [
+    'All'
+  ],
+  MaxNumberOfMessages: 1,
+  MessageAttributeNames: [
+    'All'
+  ],
+  QueueUrl: process.env.PriceQueueURL || PriceQueueURL,
+  VisibilityTimeout: 120,
+  WaitTimeSeconds: 15
+};
 
-    priceProcessor(price)
-      .then(res => done())
-      .catch((err) => {
-        console.error(err);
-        done();
-      });
-  }
-});
+const deleteSQSMessage = (message) => {
+  const deleteParams = {
+    QueueUrl: process.env.PriceQueueURL || PriceQueueURL,
+    ReceiptHandle: message.ReceiptHandle
+  };
 
-app.on('error', (err) => {
-  console.error(err.message);
-});
+  return sqs.deleteMessage(deleteParams).promise();
+};
+
+const pollPriceQueueMessage = () => {
+  sqs.receiveMessage(params).promise()
+    .then((res) => {
+      if (!res.Messages) {
+        throw new Error('No New SQS Price Message');
+      } else {
+        return res.Messages[0];
+      }
+    })
+    .then((message) => {
+      const start = Date.now();
+      statsDClient.increment('.ledger.price.received');
+      // console.log(message.MessageId);
+      // console.log(message.Attributes);
+      // console.log(message.MessageAttributes);
+      const price = JSON.parse(message.Body).payload;
+      // console.log(price);
+
+      priceProcessor(price)
+        .then((res) => {
+          statsDClient.increment('.ledger.price.processed.success');
+          statsDClient.timing('.ledger.price.processed.success.latency_ms', Date.now() - start);
+
+          return deleteSQSMessage(message);
+        })
+        .catch((err) => {
+          statsDClient.increment('.ledger.price.processed.fail');
+          statsDClient.timing('.ledger.price.processed.fail.latency_ms', Date.now() - start);
+          console.error(err);
+
+          return deleteSQSMessage(message);
+        })
+        .then(() => {
+          console.log(`Message Deleted: ${message.MessageId}`);
+          pollPriceQueueMessage();
+        })
+        .catch(() => {
+          console.error(`Message Delete Error: ${message.MessageId}`);
+          pollPriceQueueMessage();
+        });
+    })
+    .catch((err) => {
+      console.error(err.message);
+      setTimeout(pollPriceQueueMessage, 50);
+    });
+};
+
+// SQS Consumer Implementation - not fast enough
+// const app = Consumer.create({
+//   queueUrl: PriceQueueURL,
+//   attributeNames: ['All'],
+//   messageAttributeNames: ['All'],
+//   visibilityTimeout: 120,
+//   waitTimeSeconds: 15,
+//   sqs: new AWS.SQS(),
+//   handleMessage: (message, done) => {
+//     const start = Date.now();
+//     statsDClient.increment('.ledger.price.received');
+
+//     // console.log(message.MessageId);
+//     // console.log(message.Attributes);
+//     // console.log(message.MessageAttributes);
+//     const price = JSON.parse(message.Body).payload;
+//     console.log(price);
+
+//     priceProcessor(price)
+//       .then((res) => {
+//         statsDClient.increment('.ledger.price.processed.success');
+//         statsDClient.timing('.ledger.price.processed.success.latency_ms', Date.now() - start);
+//         done();
+//       })
+//       .catch((err) => {
+//         statsDClient.increment('.ledger.price.processed.fail');
+//         statsDClient.timing('.ledger.price.processed.fail.latency_ms', Date.now() - start);
+//         console.error(err);
+//         done();
+//       });
+//   }
+// });
+
+// app.on('error', (err) => {
+//   console.error(err.message);
+// });
+// app.start();
 
 if (!module.parent) {
-  app.start();
+  pollPriceQueueMessage();
 }
 
-module.exports = app;
+module.exports = pollPriceQueueMessage;
